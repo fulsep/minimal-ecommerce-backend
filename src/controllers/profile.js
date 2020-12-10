c7f6318c21b9060a0be3bf20ee698af7d058fb96
Irul <irul@impostor.local>
2020-12-10 17:57:46 +0700

    create profile controller, get profile data

---
+++ src/controllers/profile.js
@@ -0,0 +1,24 @@
+const Users = require('../models/users');
+const profile = new Users();
+
+module.exports = {
+  get: (req, res)=> {
+    // const {id} = req.authUser
+    const id = 1;
+    profile.find({where: {id}}, (err, result)=> {
+      if (result) {
+        delete result.rows[0].password;
+        return res.send({
+          success: true,
+          message: 'Profile',
+          results: result.rows[0],
+        });
+      } else {
+        return res.status(500).send({
+          success: false,
+          message: err.message,
+        });
+      }
+    });
+  },
+};
