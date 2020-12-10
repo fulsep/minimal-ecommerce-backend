4530d08c90a9b894f7b9145485a09a18f98cecef
Irul <irul@impostor.local>
2020-12-10 18:48:08 +0700

    attach auth routes to global routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const productRouter = require('./products');
 const profileRouter = require('./profile');
+const authRouter = require('./auth');
 
 const app = (app, prefix) => {
+  app.use(prefix, authRouter);
   app.use(prefix.concat('/products'), productRouter);
   app.use(prefix.concat('/profile'), profileRouter);
 };
