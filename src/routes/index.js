f0daf67f0951e47c458afd9b91e6de400eb73fab
Irul <irul@impostor.local>
2020-12-10 19:11:22 +0700

    attach authMiddleware to /profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const productRouter = require('./products');
 const profileRouter = require('./profile');
 const authRouter = require('./auth');
+const authMiddleware = require('../middlewares/auth');
 
 const app = (app, prefix) => {
   app.use(prefix, authRouter);
   app.use(prefix.concat('/products'), productRouter);
-  app.use(prefix.concat('/profile'), profileRouter);
+  app.use(prefix.concat('/profile'), authMiddleware.authCheck, profileRouter);
 };
 
 module.exports = app;
