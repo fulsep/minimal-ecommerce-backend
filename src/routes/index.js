71f994e4c61f9cd31531fbc5e04e1468f45bb426
Irul <irul@impostor.local>
2020-12-11 02:04:03 +0700

    create route address

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const productRouter = require('./products');
 const profileRouter = require('./profile');
+const addressRouter = require('./address');
 const authRouter = require('./auth');
 const authMiddleware = require('../middlewares/auth');
 
@@ -7,6 +8,7 @@ const app = (app, prefix) => {
   app.use(prefix, authRouter);
   app.use(prefix.concat('/products'), productRouter);
   app.use(prefix.concat('/profile'), authMiddleware.authCheck, profileRouter);
+  app.use(prefix.concat('/address'), authMiddleware.authCheck, addressRouter);
 };
 
 module.exports = app;
