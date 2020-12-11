ac4b50d0bd00dcc3eeca47a65e25a41f1396f5cb
Irul <irul@impostor.local>
2020-12-11 12:11:27 +0700

    attach new route to system

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const profileRouter = require('./profile');
 const flashsaleRouter = require('./flashsale');
 const addressRouter = require('./address');
 const authRouter = require('./auth');
+const cartRouter = require('./carts');
+const checkoutRouter = require('./checkout');
 const authMiddleware = require('../middlewares/auth');
 
 const app = (app, prefix) => {
@@ -11,6 +13,8 @@ const app = (app, prefix) => {
   app.use(prefix.concat('/flashsale'), flashsaleRouter);
   app.use(prefix.concat('/profile'), authMiddleware.authCheck, profileRouter);
   app.use(prefix.concat('/address'), authMiddleware.authCheck, addressRouter);
+  app.use(prefix.concat('/carts'), authMiddleware.authCheck, cartRouter);
+  app.use(prefix.concat('/checkout'), authMiddleware.authCheck, checkoutRouter);
 };
 
 module.exports = app;
