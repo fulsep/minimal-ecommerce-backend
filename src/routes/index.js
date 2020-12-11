15550b94d11b1416686af470e39dfc4feee732d8
irvanster <tovamaulana098@gmail.com>
2020-12-11 13:45:49 +0700

    change endpoint "flashsale" to "flash_sale"

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const authMiddleware = require('../middlewares/auth');
 const app = (app, prefix) => {
   app.use(prefix, authRouter);
   app.use(prefix.concat('/products'), productRouter);
-  app.use(prefix.concat('/flashsale'), flashsaleRouter);
+  app.use(prefix.concat('/flash_sale'), flashsaleRouter);
   app.use(prefix.concat('/profile'), authMiddleware.authCheck, profileRouter);
   app.use(prefix.concat('/address'), authMiddleware.authCheck, addressRouter);
 };
