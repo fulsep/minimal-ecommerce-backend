cdb7ea50e2da09d0e6bcd3f24350eb4a04c200c6
Irul <fulsep@gmail.com>
2020-12-15 20:43:51 +0700

    start from scratch

--- src/routes/carts.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable new-cap */
-const router = require('express').Router();
-
-const cartController = require('../controllers/carts');
-
-router.get('/', cartController.getList);
-router.get('/:id', cartController.directAddItem);
-module.exports = router;
--- src/routes/checkout.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable new-cap */
-const router = require('express').Router();
-
-const checkoutController = require('../controllers/checkout');
-
-router.get('/', checkoutController.getBeforeGenerate);
-router.post('/', checkoutController.generateInvoice);
-
-module.exports = router;
