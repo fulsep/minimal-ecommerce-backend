feda7e0cc5716891ef8afc15f3e7fceca13fad09
Irul <fulsep@gmail.com>
2021-01-05 10:57:44 +0700

    deactivate schema and validator

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -8,6 +8,6 @@ const cartSchema = require('../helpers/schema/carts');
 const authMiddleware = require('../middlewares/auth');
 
 router.get('/carts', authMiddleware.authCheck, checkSchema(cartSchema.list), validation, cartController.list);
-router.get('/carts/:id', authMiddleware.authCheck, checkSchema(cartSchema.addToCart), validation, cartController.addToCart);
+router.get('/carts/:id', authMiddleware.authCheck/* , checkSchema(cartSchema.addToCart), validation */, cartController.addToCart);
 
 module.exports = router;
